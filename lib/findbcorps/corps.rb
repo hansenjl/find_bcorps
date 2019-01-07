016383de9a8556c01c9073baf6a3949f26834117
github username <github email address>
2019-01-07 11:29:06 -0500

    delete commented out code that was refactored elsewhere

--- lib/findbcorps/corps.rb
+++ lib/findbcorps/corps.rb
@@ -18,21 +18,6 @@ class FindBCorps::Corp
   def self.all_listings
     @@all_listings
   end
-   
-  # # create corp objects. This also calls on #initialize
-  # def self.create_from_listings(all_listings)
-  #   all_listings.each do |corp|
-  #     corp = FindBCorps::Corp.new(corp)#calls the initialize method.
-  #   end
-  # end
-
-  # def add_profile_attributes(profile_hash)
-  #   @certified_date = profile_hash[:certified_date]
-  #   @sectors = profile_hash[:sectors]
-  #   @company_description = profile_hash[:company_description]
-  #   @website_url = profile_hash[:website_url]
-  # end
-
 end
 
 
