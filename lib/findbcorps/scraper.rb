53e7f016a0185a24268418add1d76b562e47ecba
github username <github email address>
2019-01-07 10:41:59 -0500

    create a new corp object inside self.scrape_listings and remove need for hashes in both scrape_listings and scrape_profile page methods.

--- lib/findbcorps/scraper.rb
+++ lib/findbcorps/scraper.rb
@@ -5,36 +5,24 @@ class FindBCorps::Scraper
   def self.scrape_listings
   # USA-only
     index_page = Nokogiri::HTML(open('https://bcorporation.net/directory?search=&industry=&country=United%20States&state=&city='))
-    
-    scraped_listings_array = []
    
-    #iterate over the CS classes one at a time and pull the data within each listing card(e.g. company name, location, etc). Store these into the all_listings.
-    index_page.css("div.card__inner").each do |listing_card|
-      scraped_listings_array << {
-        name: listing_card.css(".heading4.card__title").text,
-        offerings: listing_card.css(".field-name-field-products-and-services").text,
-        location: listing_card.css(".field-name-field-country").text.gsub!("Location: ", "").gsub!(", United States", ""),
-        profile_url: listing_card.css("a").attribute("href").value
-      }
+    index_page.search("div.card__inner").each do |listing|
+      
+      name =  listing.css(".heading4.card__title").text
+      offerings  =  listing.css(".field-name-field-products-and-services").text
+      location = listing.css(".field-name-field-country").text.gsub!("Location: ", "").gsub!(", United States", "")
+      profile_url = listing.css("a").attribute("href").value
+      FindBCorps::Corp.new(name, offerings, location, profile_url)
     end
-    scraped_listings_array
   end
 
  # Scrape for items in full profile page for details that the listings scrape doesn't have.
-  def self.scrape_profile_page(profile_url)
-
-    profile_page = Nokogiri::HTML(open(profile_url))
-    profile_scrape = {}#this needs to be a hash, not an array.
-
-    profile_page.css(".node-company").each do |detail|
-      profile_scrape = {
-        certified_date: detail.css(".field-name-field-date-certified").text.gsub!("Certified Since: ", ""),
-        sectors: detail.css(".field-name-field-sector").text.gsub!("Sector: ", ""),
-        company_description: detail.css(".field-name-body").text,
-        website_url: detail.css(".field-name-field-website").text
-      }
-      end
-    profile_scrape
+  def self.scrape_profile_page(corp)
+    profile_page = Nokogiri::HTML(open('https://bcorporation.net' + corp.profile_url))
+    corp.certified_date = profile_page.css(".field-name-field-date-certified").text.gsub!("Certified Since: ", "")
+    corp.company_description =  profile_page.css(".field-name-body").text
+    corp.sectors = profile_page.css(".field-name-field-sector").text.gsub!("Sector: ", "")
+    corp.website_url = profile_page.css(".field-name-field-website").text
   end
 end 
 
