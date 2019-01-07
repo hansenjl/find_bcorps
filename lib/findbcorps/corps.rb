d6daf6856115a62bba7107fc79b958f469183e93
github username <github email address>
2019-01-07 10:43:37 -0500

    remove the create_from_listings method because each corp object will be created when the listings page is scraped.

--- lib/findbcorps/corps.rb
+++ lib/findbcorps/corps.rb
@@ -6,15 +6,12 @@ class FindBCorps::Corp
  
   @@all_listings = []
 
-  def initialize(listings_hash)
-    @name = listings_hash[:name]
-    @offerings = listings_hash[:offerings]
-    @location = listings_hash[:location]
-    @profile_url = listings_hash[:profile_url]
-    # OR
-    # listings_hash.each do |key, value|
-    # self.send("#{key}=", value)
-    #basically the above is saying something like'self.name = the key value of :name.
+  def initialize(name, offerings, location, profile_url)
+    @name = name
+    @offerings = offerings
+    @location = location
+    @profile_url = profile_url
+   
     @@all_listings << self
   end
     
@@ -22,19 +19,20 @@ class FindBCorps::Corp
     @@all_listings
   end
    
-  # create corp objects. This also calls on #initialize
-  def self.create_from_listings(all_listings)
-    all_listings.each do |corp|
-      corp = FindBCorps::Corp.new(corp)#calls the initialize method.
-    end
-  end
+  # # create corp objects. This also calls on #initialize
+  # def self.create_from_listings(all_listings)
+  #   all_listings.each do |corp|
+  #     corp = FindBCorps::Corp.new(corp)#calls the initialize method.
+  #   end
+  # end
 
-  def add_profile_attributes(profile_hash)
-    @certified_date = profile_hash[:certified_date]
-    @sectors = profile_hash[:sectors]
-    @company_description = profile_hash[:company_description]
-    @website_url = profile_hash[:website_url]
-  end
+  # def add_profile_attributes(profile_hash)
+  #   @certified_date = profile_hash[:certified_date]
+  #   @sectors = profile_hash[:sectors]
+  #   @company_description = profile_hash[:company_description]
+  #   @website_url = profile_hash[:website_url]
+  # end
+
 end
 
 
